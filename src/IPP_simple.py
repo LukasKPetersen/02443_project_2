d25eab757034acee176085828fe6f747abf0fcda
s204511 <s204511@student.dtu.dk>
2025-06-19 00:51:45 +0200

    Made a simple test script for the model

--- src/IPP_simple.py
+++ src/IPP_simple.py
@@ -12,22 +12,21 @@ class SingleServerIPP:
         self.gamma2 = gamma2  # rate of state transition from OFF to ON
         self.state = init_state
         self.rng = np.random.default_rng(seed)
-        self.next_event_time = 0
-        self.next_arrival_time = self.rng.exponential(1/lambda_on) if init_state == 'ON' else float('inf')
+        self.curr_time = 0
         self.set_sojourn_time()
 
     def set_sojourn_time(self):
         transition_rate = self.gamma1 if self.state == 'ON' else self.gamma2
-        self.curr_sojourn_time = self.next_event_time + self.rng.exponential(1/transition_rate)
+        self.next_transition = self.curr_time + self.rng.exponential(1/transition_rate)
 
     def generate_arrivals(self):
         """
         Whenever the state is ON, generate all arrivals until the next state change.
         """
         arrivals = []
-        while self.next_event_time < self.curr_sojourn_time:
-            arrivals.append(self.next_event_time)
-            self.next_event_time += self.rng.exponential(1/self.lambda_on)
+        while self.curr_time < self.next_transition:
+            arrivals.append(self.curr_time)
+            self.curr_time += self.rng.exponential(1/self.lambda_on)
         return arrivals
 
     def run(self, run_time=10000):
@@ -35,13 +34,14 @@ class SingleServerIPP:
         Run the IPP process for a specified duration.
         """
         arrivals = []
-        while self.next_event_time < run_time:
+        while self.curr_time < run_time:
             if self.state == 'ON':
                 arrivals.extend(self.generate_arrivals())
+                self.curr_time = self.next_transition
                 self.state = 'OFF'
-                self.next_event_time = self.curr_sojourn_time
+                self.set_sojourn_time()
             else:
+                self.curr_time = self.next_transition
                 self.state = 'ON'
                 self.set_sojourn_time()
-                self.next_event_time = self.curr_sojourn_time
         return arrivals
---
+++ src/simulation_test.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+from IPP_simple import SingleServerIPP
+
+intensity = 0.1  # intensity of arrivals when ON
+gamma1 = 0.5  # rate of state transition from ON to OFF
+gamma2 = 0.1  # rate of state transition from OFF to ON
+runtime = 1000  # total simulation time
+
+simulator = SingleServerIPP(lambda_on=intensity, gamma1=gamma1, gamma2=gamma2)
+arrivals = simulator.run(run_time=runtime)
+
+# some plots
+plt.figure(figsize=(10, 5))
+plt.hist(arrivals, bins=50, color='blue', alpha=0.7)
+plt.title('Histogram of Arrivals in Single Server IPP Simulation')
+plt.xlabel('Time')
+plt.ylabel('Frequency')
+plt.xlim(0, runtime)
+plt.grid()
+plt.show()
+
+plt.figure(figsize=(10, 5))
+plt.plot(arrivals, range(len(arrivals)), marker='o', linestyle='-', color='blue', alpha=0.7)
+plt.title('Arrival Times in Single Server IPP Simulation')
+plt.xlabel('Time')
+plt.ylabel('Arrival Index')
+plt.xlim(0, runtime)
+plt.grid()
+plt.show()
